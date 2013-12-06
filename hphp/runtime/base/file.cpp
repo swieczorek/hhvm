f37207f1c2c12c72e8bd28f8fbc99a135f177135
Daniel Sloof <goapsychadelic@gmail.com>
2013-12-06 15:24:01 +0100

    fix stream_get_contents when no offset is specified
    File does its own internal buffering and uses m_readpos, m_writepos,
    m_position for the purpose of keeping track of file offsets. This means
    that current (PHP) position is not in sync with the underlying file
    descriptor (m_fd).
    
    This works fine, were it not for the fact that ext_stream calls directly
    into pure virtual method readImpl that generally lives in PlainFile
    and works directly on the file descriptor that might contain an
    invalid (buffered-ahead) position.
    
    This pull request does several things:
    - Remove default value 0 for File::read(int64_t length), since that
      implementation returns earlier for 0 values anyway.
    - Implement an overloaded method File::read() that reads the remainder
      of the file (until EOF) by using proper File::m_* properties.
    - Make ext_stream call into File::read instead of PlainFile::readImpl.

--- hphp/runtime/base/file.cpp
+++ hphp/runtime/base/file.cpp
@@ -193,6 +193,40 @@ int File::getc() {
   return (int)(unsigned char)buffer[0];
 }
 
+String File::read() {
+  String s = String(CHUNK_SIZE, ReserveString);
+  char *ret = s.bufferSlice().ptr;
+  int64_t copied = 0;
+  int64_t avail = bufferedLen();
+
+  while (!eof()) {
+    if (m_buffer == nullptr) {
+      m_buffer = (char *)malloc(CHUNK_SIZE);
+    }
+
+    if (avail > 0) {
+      memcpy(ret + copied, m_buffer + m_readpos, avail);
+      copied += avail;
+    }
+
+    m_writepos = readImpl(m_buffer, CHUNK_SIZE);
+    m_readpos = 0;
+    avail = bufferedLen();
+
+    if (avail == 0 || m_nonblocking) {
+      // For nonblocking mode, temporary out of data.
+      break;
+    }
+
+    if (copied + avail >= s->capacity()) {
+      s.reserve(s->capacity() + CHUNK_SIZE);
+    }
+  }
+
+  m_position += copied;
+  return s.setSize(copied);
+}
+
 String File::read(int64_t length) {
   if (length <= 0) {
     raise_notice("Invalid length %" PRId64, length);
